3a0ac4ddf754f96200c33a4936ea2c4829331664
Sangbum Kim <bearksb1115@gmail.com>
2022-12-10 16:52:54 +0900

    Add failing test

--- test_model.py
+++ test_model.py
@@ -1,6 +1,8 @@
 from datetime import date, timedelta
 import pytest
 
+from model import Batch, OrderLine
+
 # from model import ...
 
 today = date.today()
@@ -9,19 +11,37 @@ later = tomorrow + timedelta(days=10)
 
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail("todo")
+    init_quantity = 20
+    order_line_quantity = 2
+    batch = Batch(1, "SMALL-TABLE", init_quantity)
+    order_line = OrderLine("SMALL-TABLE", order_line_quantity)
+    batch.allocate(order_line)
+    assert batch.quantity == init_quantity - order_line_quantity
 
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    init_quantity = 20
+    order_line_quantity = 2
+    batch = Batch(1, "SMALL-TABLE", init_quantity)
+    order_line = OrderLine("SMALL-TABLE", order_line_quantity)
+    batch.allocate(order_line)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    init_quantity = 20
+    order_line_quantity = 22
+    batch = Batch(1, "SMALL-TABLE", init_quantity)
+    order_line = OrderLine("SMALL-TABLE", order_line_quantity)
+    with pytest.raises(Exception):
+        batch.allocate(order_line)
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    init_quantity = 20
+    order_line_quantity = 20
+    batch = Batch(1, "SMALL-TABLE", init_quantity)
+    order_line = OrderLine("SMALL-TABLE", order_line_quantity)
+    batch.allocate(order_line)
 
 
 def test_prefers_warehouse_batches_to_shipments():
