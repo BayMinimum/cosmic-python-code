92601a5436f8df10c07b787345756f68b167e1df
Sangbum Kim <bearksb1115@gmail.com>
2022-12-10 17:03:41 +0900

    Implement allocate logic to pass quantity tests

--- model.py
+++ model.py
@@ -1,11 +1,15 @@
-from typing import List
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List, Optional
 
 
+@dataclass
 class OrderLine:
     sku: str
     quantity: int
 
 
+@dataclass
 class Order:
     reference: int
     order_lines: List[OrderLine]
@@ -15,3 +19,18 @@ class Batch:
     reference_number: int
     sku: str
     quantity: int
+
+    def __init__(self, 
+                 reference_number: int,
+                 sku: str,
+                 quantity: int) -> None:
+        self.reference_number = reference_number
+        self.sku = sku
+        self.quantity = quantity
+    
+    def allocate(self, order_line: OrderLine) -> None:
+        if self.quantity >= order_line.quantity:
+            self.quantity -= order_line.quantity
+        else:
+            raise ValueError("Available quantity in batch is less than OrderLine quatity")
+        
